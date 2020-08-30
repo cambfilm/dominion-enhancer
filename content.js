0afec37eb1f00627fe7a7d9e353e4c0f1b432cc2
camburnett11@gmail.com <camburnett11@gmail.com>
2020-08-29 23:58:41 -0400

    saucy style

--- content.js
+++ content.js
@@ -1,38 +1,64 @@
-$(window).on('load', function() {
-    
-    setTimeout(function() {
-        $('.mini-card').hover(function() {
-            var $card = $(this);
-            var $art = $card.find('.mini-card-art');
-            console.log($art.css('background-image'));
-        });
-    }, 5000);
+var AUDIO = {
+    gasp: 'https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-blastwave-fx/Blastwave_FX_CrowdGasp_BW.61631.mp3',
+    horns: 'https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-21014/zapsplat_sport_air_horn_soccer_blast_tune_21154.mp3',
+    saucy: [
+        'https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-14566/zapsplat_human_voice_male_english_saucy_flirtatious_ohh_hello_001_17491.mp3',
+        'https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-14566/zapsplat_human_voice_male_english_saucy_flirtatious_ohh_hello_002_17492.mp3',
+        'https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-14566/zapsplat_human_voice_male_english_saucy_flirtatious_ohh_hello_003_17493.mp3'
+    ]
+};
+var IMAGES = {
+    chancellor: 'https://i.imgur.com/R3Swjnq.jpg'
+};
+var saucy_interval = null;
+var SAUCY_TIME = 270000; //  3 mins.
 
+$.fn.random = function() {
+    return this.eq(Math.floor(Math.random() * this.length));
+};
+
+$(window).on('load', function() {
     var $settings_window = $('<div id="de-popup"></div>');
-    $settings_window.css( {
-        position: 'absolute',
-        bottom: '0',
-        left: '0',
-        width: '120px',
-        height: '40px',
-        backgroundColor: '#fff',
-        zIndex: '9999999',
-        cursor: 'pointer'
-    });
     var $window_text = $('<span>Chancellor Mode</span>');
     $settings_window.click(function() {
         enableChancellorMode();
+    });
+    $settings_window.mouseover(function() {
+        var audio = new Audio(AUDIO.gasp);
+        audio.play();
     });
     $settings_window.append($window_text);
     $('body').append($settings_window);
 });
 
 function enableChancellorMode() {
-    var chancellor = 'https://i.imgur.com/R3Swjnq.jpg';
+    var chancellor = IMAGES.chancellor;
     var $art = $('.mini-card').find('.mini-card-art');
-    console.log($art.css('background-image'));
-    $art.css('background-image', 'url(' + chancellor + ')');
+    // $art.css('background-image', 'url(' + chancellor + ')');
 
-    var audio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-21014/zapsplat_sport_air_horn_soccer_blast_tune_21154.mp3');
+    var audio = new Audio(AUDIO.horns);
     audio.play();
+
+    var saucies = [];
+
+    saucy_interval = setInterval(() => {
+        var saucy_audio = new Audio(getSaucy());
+        saucy_audio.play();
+        console.log('start saucy');
+        
+        var $saucy_chancellor = $art.random(saucies);
+        saucies.push($saucy_chancellor);
+
+        $saucy_chancellor.addClass('saucy');
+        $saucy_chancellor.css('background-image', 'url(' + chancellor + ')');
+
+        $(saucy_audio).on('ended', function() {
+            $saucy_chancellor.removeClass('saucy');
+            $saucy_chancellor.css('background-image', 'url(' + nonsaucy_bg + ')');
+        });
+    }, SAUCY_TIME);
+}
+
+function getSaucy() { 
+    return AUDIO.saucy[ AUDIO.saucy.length * Math.random() << 0 ];
 }
